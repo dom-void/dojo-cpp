5ff5aefbeae4bece1e794b9396feb40c71bf2baa
dom:V0iD <doe@onet.pl>
2024-07-13 11:07:17 +0200

    Create count-to.cpp

---
+++ count-to.cpp
@@ -0,0 +1,15 @@
+// This code showed me how much faster C++ is campered to JS or Python
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  unsigned int n;
+  
+  cout << "Let me count to: ";
+  cin >> n;
+  
+  if (n > 0) n--;
+  
+  cout << "Done!";
+}
