8a50d42a0e3b02dff8c50d828015f7dc6d88d9dd
dom:V0iD <doe@onet.pl>
2024-07-16 11:02:57 +0200

    Update fibonacci-generator.cpp

--- fibonacci-generator.cpp
+++ fibonacci-generator.cpp
@@ -2,37 +2,35 @@
 #include <iomanip>
 
 #define COLUMNS 5
-#define SPACE 13
+#define SPACE 20
 
 using namespace std;
 
 int main()
 {
-  // generates up to 48 correct Fibonacci numbers:
-  // unsigned int prev, curr, next;
-  
-  // Generates rounded Fibonacci numbers starting from 32nd:
-  long double prev, curr, next;
-  
-  int counter, n;
-  
-  cout << "Number of Fibonacci numbers: " << endl;
-  cin >> n;
-  
-  prev = 0;
-  curr = 1;
-  
-  cout << setw(SPACE) << prev << setw(SPACE) << curr;
-  
-  for (counter = 3; counter <= n; counter++) {
-      next = prev + curr;
-      prev = curr;
-      curr = next;
-      
-      cout << setw(SPACE) << curr;
-      
-      if (counter % COLUMNS == 0) {
+    unsigned long long int prev, curr, next;
+    
+    int counter, n;
+    
+    cout << "Number of Fibonacci numbers: " << endl;
+    cin >> n;
+    
+    prev = 0;
+    curr = 1;
+    
+    cout << setw(SPACE) << prev << setw(SPACE) << curr;
+    
+    if (n > 90) n = 90;
+    
+    for (counter = 3; counter <= n; counter++) {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+        
+        cout << setw(SPACE) << curr;
+        
+        if (counter % COLUMNS == 0) {
           cout << endl;
-      }
-  }
+        }
+    }
 }
